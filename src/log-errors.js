f3af3c40ba9bddc1d42cc15f94cc099ea35c9956
Mark R Henderson <mark@mrh.io>
2019-05-05 16:02:04 -0400

    patch:adding NeedToUpgrade error

--- src/log-errors.js
+++ src/log-errors.js
@@ -3,9 +3,11 @@
 const ImmutableDBNotDefinedError = () => new Error('ImmutableDB instance not defined')
 const LogNotDefinedError = () => new Error('Log instance not defined')
 const NotALogError = () => new Error('Given argument is not an instance of Log')
+const NeedToUpgrade = () => new Error('OrbitDB: append is now disabled in 0.19. Please upgrade to the newest version of OrbitDB')
 
 module.exports = {
   ImmutableDBNotDefinedError: ImmutableDBNotDefinedError,
   LogNotDefinedError: LogNotDefinedError,
   NotALogError: NotALogError,
+  NeedToUpgrade: NeedToUpgrade
 }
