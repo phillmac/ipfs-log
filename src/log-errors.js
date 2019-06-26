ff2d09bf93a0ffbe6866b4db4fd75397cfc32c8e
phillmac <phillmac@users.noreply.github.com>
2019-06-26 18:32:59 +1000

    Add DisallowedError

--- src/log-errors.js
+++ src/log-errors.js
@@ -6,10 +6,13 @@ const NotALogError = () => new Error('Given argument is not an instance of Log')
 const CannotJoinWithDifferentId = () => new Error('Can\'t join logs with different IDs')
 const LtOrLteMustBeStringOrArray = () => new Error('lt or lte must be a string or array of Entries')
 
+class DisallowedError extends Error {}
+
 module.exports = {
   IPFSNotDefinedError: IPFSNotDefinedError,
   LogNotDefinedError: LogNotDefinedError,
   NotALogError: NotALogError,
   CannotJoinWithDifferentId: CannotJoinWithDifferentId,
-  LtOrLteMustBeStringOrArray: LtOrLteMustBeStringOrArray
+  LtOrLteMustBeStringOrArray: LtOrLteMustBeStringOrArray,
+  DisallowedError: DisallowedErrorAd
 }
